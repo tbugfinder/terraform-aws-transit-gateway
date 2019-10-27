54d9d910ed9f6420133ceb83ed66cff32b62b5c4
thebugfinder <github@online.ms>
2019-10-27 19:33:34 +0100

    remove comments

--- resource_share.tf
+++ resource_share.tf
@@ -7,17 +7,12 @@ resource "aws_ram_resource_share" "ramshare" {
 
 # Associate principals with share
 resource "aws_ram_principal_association" "ram_principal_assoc" {
-  #  principal          = "394197307369"
   principal          = "${data.aws_caller_identity.spoke.account_id}"
   resource_share_arn = "${aws_ram_resource_share.ramshare.id}"
 }
 
 # Associate resources with share
 resource "aws_ram_resource_association" "ram_resource_assoc" {
-  # count cannot be computed while using resource outputs as input
-  # helper using null datasource to convert string to number - weird but a workaround
-  #  count = "${data.null_data_source.values.outputs["arnscount"]}"
-  resource_arn = "${aws_ec2_transit_gateway.test-tgw.arn}"
-
+  resource_arn       = "${aws_ec2_transit_gateway.test-tgw.arn}"
   resource_share_arn = "${aws_ram_resource_share.ramshare.id}"
 }
